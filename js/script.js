a2e33f7ff7580d852f7839cb2c8e1e96368cecaa
Malwina Leśniewska <malwinalesniewska@gazeta.pl>
2022-03-31 11:57:45 +0200

    add first variable

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+var computerMove, playerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
