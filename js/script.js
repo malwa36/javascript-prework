e89a8cedde87f823316ae4cd911d2b99685b9f41
Malwina Leśniewska <malwinalesniewska@gazeta.pl>
2022-03-31 09:50:54 +0200

    add first message

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
