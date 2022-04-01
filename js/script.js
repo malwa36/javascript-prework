aa995a4c2015ebcbb08035d4dbecfd65a75ebffa
Malwina Leśniewska <malwinalesniewska@gazeta.pl>
2022-04-01 10:36:32 +0200

    change script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
-var computerMove, playerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+}
+if (randomNumber == '2') {
+  computerMove = 'papier';
+}
+if (randomNumber == '3') {
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
