e5e9392f9b1dd67784aa730d34c4066f029b06c0
Malwina Leśniewska <malwinalesniewska@gazeta.pl>
2022-04-04 13:42:37 +0200

    let computerMove

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function buttonClicked(argButtonName) {
   let playerMove = argButtonName;
   const randomNumber = Math.floor(Math.random() * 3 + 1);
   console.log('wylosowana liczba to: ' + randomNumber);
-  computerMove = getMoveName(randomNumber);
+  let computerMove = getMoveName(randomNumber);
   console.log('ruch komputera to: ' + computerMove);
   displayResult(playerMove, computerMove);
 }
